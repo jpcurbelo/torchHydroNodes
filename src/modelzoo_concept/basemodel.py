Fix scaling in poretrainer and trainer

src/modelzoo_concept/basemodel.py
@@ -27,8 +27,8 @@ class BaseConceptModel:
         # Time series
         self.time_series = np.linspace(0, len(ds['date'].values) - 1, len(ds['date'].values))
         
-        # Interpolators
-        self.interpolators = self.create_interpolator_dict()
+        # # Interpolators
+        # self.interpolators = self.create_interpolator_dict()
         
     def conceptual_model(self, t, y, params):
         '''This function should implement the conceptual model to be used for the task'''

src/modelzoo_concept/exphydro.py
@@ -8,13 +8,13 @@ from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
+import torch
 
 from src.modelzoo_concept.basemodel import BaseConceptModel
 from src.utils.load_process_data import (
     Config,
     ExpHydroCommon,
 )
-
 # Ref: exphydro -> https://hess.copernicus.org/articles/26/5085/2022/
 class ExpHydro(BaseConceptModel, ExpHydroCommon):
     
@@ -36,6 +36,8 @@ class ExpHydro(BaseConceptModel, ExpHydroCommon):
         
         # Parameters per basin
         self.params_dict = self.get_parameters()
+
+        self.eps = torch.finfo(cfg.precision['torch']).eps
         
     def conceptual_model(self, t, y, f, smax, qmax, df, tmax, tmin):
         
@@ -54,18 +56,40 @@ class ExpHydro(BaseConceptModel, ExpHydroCommon):
         s1 = y[1]
 
         # Interpolate the input variables
-        precp = self.precp_interp(t, extrapolate='periodic')
-        temp = self.temp_interp(t, extrapolate='periodic')
-        lday = self.lday_interp(t, extrapolate='periodic')
+        # precp = self.precp_interp(t, extrapolate='periodic')
+        # temp = self.temp_interp(t, extrapolate='periodic')
+        # lday = self.lday_interp(t, extrapolate='periodic')
+        precp = self.precp_interp(t, )
+        temp = self.temp_interp(t, )
+        lday = self.lday_interp(t, )
 
         # Compute and substitute the 5 mechanistic processes
         q_out = Qb(s1, f, smax, qmax, self.step_function) + Qs(s1, smax, self.step_function)
         m_out = M(s0, temp, df, tmax, self.step_function)
+
+        # print('s0', s0)
+        # print('s1', s1)
+        # print('f', f)
+        # print('Smax', smax)
+        # print('Qmax', qmax)
+        # print('Df', df)
+        # print('Tmax', tmax)
+        # print('Tmin', tmin)
+        # print('precp', precp)
+        # print('temp', temp)
+        # print('lday', lday)
+        # print('Q_out', q_out)
+        # print('M_out', m_out)
         
         # Eq 1 - Hoge_EtAl_HESS_2022
         ds0_dt = Ps(precp, temp, tmin, self.step_function) - m_out
         # Eq 2 - Hoge_EtAl_HESS_2022
         ds1_dt = Pr(precp, temp, tmin, self.step_function) + m_out - ET(s1, temp, lday, smax, self.step_function) - q_out
+
+        
+        # print('dS0_dt', ds0_dt, type(ds0_dt))
+        # print('dS1_dt', ds1_dt, type(ds1_dt))
+        # aux = input('Press Enter to continue...')
 
         return [ds0_dt, ds1_dt]
            
@@ -101,19 +125,28 @@ class ExpHydro(BaseConceptModel, ExpHydroCommon):
         # Set the parameters
         params = tuple(basin_params[2:])
 
+        # print('params:', params)
+        # print('y0:', y0)
+        # print('t_span:', (0, self.precp.shape[1] - 1))
+        # print('t_eval:', self.time_series)
+        # print('method:', self.odesmethod)
+
+        # aux = input("Press Enter to continue...")
+
         # Run the model
         y = sp_solve_ivp(self.conceptual_model, t_span=(0, self.precp.shape[1] - 1), y0=y0, t_eval=self.time_series, 
                          args=params, 
-                         method=self.odesmethod,
+                        #  method=self.odesmethod,
+                        method='LSODA',
                         #  method='DOP853',
-                        # rtol=1e-9, atol=1e-12,
+                        rtol=1e-9, atol=1e-12,
                     )
         
         # Extract snow and water series -> two state variables representing buckets for snow and water Hoge_EtAl_HESS_2022
         s_snow = y.y[0]
         s_water = y.y[1]
-        s_snow = np.maximum(s_snow, 0)
-        s_water = np.maximum(s_water, 0)
+        # s_snow = np.maximum(s_snow, 0)
+        # s_water = np.maximum(s_water, 0)
 
         # Unpack the parameters to call the mechanistic processes
         f, smax, qmax, df, tmax, tmin = params
@@ -121,19 +154,19 @@ class ExpHydro(BaseConceptModel, ExpHydroCommon):
         # Calculate the mechanistic processes -> q_bucket, et_bucket, m_bucket, ps_bucket, pr_bucket
         # Discharge
         q_bucket = Qb(s_water, f, smax, qmax, self.step_function) + Qs(s_water, smax, self.step_function)
-        q_bucket = np.maximum(q_bucket, 0)
+        q_bucket = np.maximum(q_bucket, self.eps)
         # Evapotranspiration
         et_bucket = ET(s_water, self.temp_basin, self.lday_basin, smax, self.step_function)
-        et_bucket = np.maximum(et_bucket, 0)
+        et_bucket = np.maximum(et_bucket, self.eps)
         # Melting
         m_bucket = M(s_snow, self.temp_basin, df, tmax, self.step_function)
-        m_bucket = np.maximum(m_bucket, 0)
+        m_bucket = np.maximum(m_bucket, self.eps)
         # Precipitation as snow
         ps_bucket = Ps(self.precp_basin, self.temp_basin, tmin, self.step_function)
-        ps_bucket = np.maximum(ps_bucket, 0)
+        # ps_bucket = np.maximum(ps_bucket, 0)
         # Precipitation as rain
         pr_bucket = Pr(self.precp_basin, self.temp_basin, tmin, self.step_function)
-        pr_bucket = np.maximum(pr_bucket, 0)
+        # pr_bucket = np.maximum(pr_bucket, 0)
         
         # Mind this order for 'save_results' method - the last element is the target variable (q_obs)
         return s_snow, s_water, et_bucket, m_bucket, ps_bucket, pr_bucket, q_bucket
@@ -238,22 +271,25 @@ Pr = lambda p, temp, tmin, step_fct: step_fct(temp - tmin) * p
 # M = lambda temp, tmax, s0, df, step_fct: step_fct(temp - tmax) * step_fct(s0) * np.minimum(s0, df * (temp - tmax))
 def M(s0, temp, df, tmax, step_fct):
     
-    if np.isscalar(temp) and np.isscalar(s0):
-        if temp > tmax and s0 > 0:
-            return  step_fct(np.minimum(s0, df * (temp - tmax)))
-        else:
-            return 0.0
-    else:
-        # If temp and s0 are arrays, perform element-wise calculation
-        result = np.zeros_like(temp)  # Initialize result array with zeros
+    # if np.isscalar(temp) and np.isscalar(s0):
+    #     if temp > tmax and s0 > 0:
+    #         return  step_fct(np.minimum(s0, df * (temp - tmax)))
+    #     else:
+    #         return 0.0
+    # else:
+    #     # If temp and s0 are arrays, perform element-wise calculation
+    #     result = np.zeros_like(temp)  # Initialize result array with zeros
         
-        # Check condition for each element of the arrays
-        condition = np.logical_and(temp > tmax, s0 > 0)
+    #     # Check condition for each element of the arrays
+    #     condition = np.logical_and(temp > tmax, s0 > 0)
 
-        # Calculate result for elements satisfying the condition
-        result[condition] = np.minimum(s0[condition], df * (temp[condition] - tmax))
+    #     # Calculate result for elements satisfying the condition
+    #     result[condition] = np.minimum(s0[condition], df * (temp[condition] - tmax))
 
-        return result
+    #     return result
+
+    return step_fct(temp - tmax) * step_fct(s0) * np.minimum(s0, df * (temp - tmax))
+
 
 # Evapotranspiration (A3) - Hoge_EtAl_HESS_2022
 ET = lambda s1, temp, lday, smax, step_fct: step_fct(s1) * step_fct(s1 - smax) * PET(temp, lday)  \

src/modelzoo_hybrid/basetrainer.py
@@ -156,14 +156,26 @@ class BaseHybridModelTrainer:
 
                 for dsp in self.ds_periods:
 
+                    # if dsp != 'ds_train':
+                    #     continue
+
                     period_name = dsp.split('_')[-1]
 
                     ds_period = getattr(self.model.pretrainer.fulldataset, dsp)
                     ds_basin = ds_period.sel(basin=basin)
 
+                    # print('dsp:', dsp)
+                    # print('ds_basin:', ds_basin)
+
+                    # aux = input("Press Enter to continue...")
+
                     time_series = ds_basin['date'].values
-                    # time_idx = torch.linspace(0, len(time_series) - 1, len(time_series), dtype=self.model.data_type_torch)
-                    time_idx = np.linspace(0, len(time_series) - 1, len(time_series), dtype=self.model.data_type_np)
+                    # If the period is not ds_train, then shift the time series by the length of ds_train
+                    if dsp == 'ds_train':        
+                        time_idx = np.linspace(0, len(time_series) - 1, len(time_series), dtype=self.model.data_type_np)
+                    else:
+                        time_idx = np.linspace(len(self.model.dataset['date'].values) - len(time_series), len(self.model.dataset['date'].values) - 1, len(time_series), dtype=self.model.data_type_np)
+
                     input_var_names = self.model.pretrainer.input_var_names + ['time_idx']
 
                     # Add time_idx to the dataset, making sure to match the correct dimensions

src/modelzoo_hybrid/expohydroM100.py
@@ -36,6 +36,9 @@ class ExpHydroM100(BaseHybridModel, ExpHydroCommon, nn.Module):
 
         # Parameters per basin
         self.params_dict = self.get_parameters()
+
+        if self.cfg.scale_target_vars:
+            self.scale_target_vars(is_trainer=True)
     
 
     def forward(self, inputs, basin):
@@ -76,9 +79,8 @@ class ExpHydroM100(BaseHybridModel, ExpHydroCommon, nn.Module):
         # #  
         # #                )
         ode_solver = torchdiffeq.odeint
-        # S_sol = ode_solver(self.hydro_odes_M100, y0=S_ic, t=time_series, method='euler', rtol=1e-3, atol=1e-6) 
         y = ode_solver(self.hybrid_model, y0=y0, t=time_series, method=self.odesmethod, rtol=1e-3, atol=1e-6)   # 'rk4' 'midpoint'   'euler' 'dopri5' #rtol=1e-6, atol=1e-6
-        
+        # y = ode_solver(self.hybrid_model, y0=y0, t=time_series, method='rk4', rtol=1e-3, atol=1e-6)
         
         # Update the state variables from the ODE solution
         s_snow_nn  = y[:, 0, 0]
@@ -141,15 +143,28 @@ class ExpHydroM100(BaseHybridModel, ExpHydroCommon, nn.Module):
         # aux = input("Press Enter to continue...")
 
         # Compute ET from the pretrainer.nnmodel
-        inputs_nn = torch.stack([s0, s1, temp, precp], dim=-1)
+        # # inputs_nn = torch.stack([s0, s1, temp, precp], dim=-1)
+        inputs_nn = torch.stack([s0, s1, precp, temp], dim=-1)
 
         basin = self.basin
         if not isinstance(basin, list):
             basin = [basin]
-        
+
         m100_outputs = self.pretrainer.nnmodel(inputs_nn, basin)[0] #.to(self.device)
         # m100_outputs = self.pretrainer.nnmodel(inputs_nn)[0]
-        # Target variables:  Psnow, M, Prain, ET and, Q
+
+        # print('inputs_nn', inputs_nn)
+        # print('t', t)
+        # print('s0', s0)
+        # print('s1', s1)
+        # print('precp', precp)
+        # print('temp', temp)
+        # print('lday', lday)
+        # print('m100_outputs.shape', m100_outputs.shape)
+        # print('m100_outputs', m100_outputs)
+        # aux = input("Press Enter to continue AAA...")
+
+        # Target variables:  Psnow, Prain, M, ET and, Q
         p_snow, p_rain, m, et, q = m100_outputs[0], m100_outputs[1], m100_outputs[2], \
                                    m100_outputs[3], m100_outputs[4]
         
@@ -160,6 +175,14 @@ class ExpHydroM100(BaseHybridModel, ExpHydroCommon, nn.Module):
             m = torch.relu(self.step_function(s0) * torch.sinh(m))
             et = self.step_function(s1) * torch.exp(et) * lday
             q = self.step_function(s1) * torch.exp(q) 
+
+            # p_snow = torch.relu(torch.sinh(p_snow))
+            # p_rain = torch.relu(torch.sinh(p_rain))
+            # m = torch.relu(torch.sinh(m))
+            # et = torch.relu(torch.exp(et) * lday)
+            # q = torch.relu(torch.exp(q))
+
+    
 
         # # # Relu the Mechanism Quantities
         # # p_snow = torch.relu(p_snow)

src/modelzoo_nn/basepretrainer.py
@@ -78,9 +78,37 @@ class NNpretrainer(ExpHydroCommon):
         self.input_var_names = self.cfg.nn_dynamic_inputs
         self.output_var_names = self.cfg.nn_mech_targets
 
+
+        # et_values = self.dataset['et_bucket'].values
+        # q_values = self.dataset['q_bucket'].values
+        # m_values = self.dataset['m_bucket'].values
+        # ps_values = self.dataset['ps_bucket'].values
+        # pr_values = self.dataset['pr_bucket'].values
+
+        # print("self.ET_mech", et_values[:5], et_values[-5:])
+        # print("self.Q_mech", q_values[:5], q_values[-5:])
+        # print("self.M_mech", m_values[:5], m_values[-5:])
+        # print("self.P_snow", ps_values[:5], ps_values[-5:])
+        # print("self.P_rain", pr_values[:5], pr_values[-5:])
+
+
         # Scale the target variables
         if self.cfg.scale_target_vars:
-            self.dataset = self.scale_target_vars(is_trainer=False)
+            self.scale_target_vars(is_trainer=False)
+
+        # et_values = self.dataset['et_bucket'].values
+        # q_values = self.dataset['q_bucket'].values
+        # m_values = self.dataset['m_bucket'].values
+        # ps_values = self.dataset['ps_bucket'].values
+        # pr_values = self.dataset['pr_bucket'].values
+
+        # print("self.ET_mech", et_values[:5], et_values[-5:])
+        # print("self.Q_mech", q_values[:5], q_values[-5:])
+        # print("self.M_mech", m_values[:5], m_values[-5:])
+        # print("self.P_snow", ps_values[:5], ps_values[-5:])
+        # print("self.P_rain", pr_values[:5], pr_values[-5:])
+        
+        # aux = input("Press Enter to continue...")
 
         # Create the dataloader
         self.dataloader = self.create_dataloaders()
@@ -154,6 +182,8 @@ class NNpretrainer(ExpHydroCommon):
             tensor_dict['time_idx'] = time_idx
         else:
             input_var_names = self.input_var_names
+
+        # print("input_var_names:", input_var_names)
         
         input_tensors = [tensor_dict[var] for var in input_var_names]
         output_tensors = [tensor_dict[var] for var in self.output_var_names]
@@ -212,6 +242,9 @@ class NNpretrainer(ExpHydroCommon):
 
                 # Move targets to the same device as predictions
                 targets = targets.to(self.device)
+
+                # print(predictions.shape, targets.shape)
+                # aux = input("Press Enter to continue...")
 
                 # Find the indices of NaNs in the predictions
                 nan_mask = torch.isnan(predictions)
@@ -307,13 +340,13 @@ class NNpretrainer(ExpHydroCommon):
                 if self.cfg.verbose and current_lr != new_lr:
                     print(f"Learning rate updated to {new_lr}")
 
-        # # Save the final model weights and plots
-        # if self.cfg.verbose:
-        #     print("-- Saving final model weights --")
-        # self.save_model()
-        # if self.cfg.verbose:
-        #     print("-- Saving final plots --")
-        # self.save_plots()
+        # Save the final model weights and plots
+        if self.cfg.verbose:
+            print("-- Saving final model weights --")
+        self.save_model()
+        if self.cfg.verbose:
+            print("-- Saving final plots --")
+        self.save_plots()
         if self.cfg.verbose:
             print("-- Evaluating the model --")
         self.evaluate()

src/thn_run.py
@@ -179,21 +179,29 @@ def train_hybrid_model(config_file: Path, gpu: int = None):
     model_nn = get_nn_model(model_concept)
 
     print(f'-- Neural network model: {model_nn.__class__.__name__}')
+    # print(model_nn)
 
     # Load the neural network model state dictionary if cfg.nn_model_dir exists
     if cfg.nn_model_dir is not None:
+
+        print(f'-- Loading the neural network model state dictionary from {cfg.nn_model_dir}')
 
         pattern = 'pretrainer_*basins.pth'
         model_path = Path(project_dir) / 'data' / cfg.nn_model_dir / 'model_weights'
         # Find the file(s) matching the pattern
         matching_files = list(model_path.glob(pattern))
         model_file = matching_files[0]
+        # model_file = '1013500_modelM100_leakyrelu.pth'
+        print(f'-- Loading the model weights from {model_file}')
         # Load the neural network model state dictionary
         model_file = model_path / model_file
         # Load the state dictionary from the saved model
         state_dict = torch.load(model_file)
+        # print('state_dict:', state_dict)
         # Load the state dictionary into the model
         model_nn.load_state_dict(state_dict)
+
+    # aux = input("Press Enter to continue...")
 
     # Pretrainer
     pretrainer = get_nn_pretrainer(model_nn, dataset)
@@ -242,7 +250,7 @@ def resume_training(run_dir: Path, epoch: int, gpu: int = None):
 
 # Example usage:
 # python thn_run.py conceptual --config-file ../examples/config_run_m0.yml
-# python thn_run.py pretrainer --action train --config-file ../examples/config_run_nn_pre_testing.yml
+# python thn_run.py pretrainer --action train --config-file ../examples/config_run_nn_pre.yml
 # python thn_run.py pretrainer --action evaluate --run-dir ../examples/runs/pretrainer_run_240530_105452
 # python thn_run.py hybrid --action train --config-file ../examples/config_run_hybrid.yml
 if __name__ == "__main__":

src/utils/load_process_data.py
@@ -683,6 +683,10 @@ class ExpHydroCommon:
                                 
                 # Get the variable values
                 var_values = self.dataset[var].sel(basin=basin).values
+
+                # print('self.time_series:', self.time_series)
+                # print('var_values:', var_values)
+                # aux = input("Press Enter to continue...")
                 
                 # Interpolate the variable values
                 interpolators[basin][var] = Akima1DInterpolator(self.time_series, var_values)
@@ -738,46 +742,32 @@ class ExpHydroCommon:
 
     def scale_target_vars(self, is_trainer=False):
        
-        epsilon = 1e-10  # Small constant to avoid division by zero and log of zero
+        epsilon = 1e-16  # Small constant to avoid division by zero and log of zero
+
+        # print(self.dataset)
 
         # Scale the target variables
-        ## Psnow -> arcsinh
-        self.dataset['ps_bucket'] = (('basin', 'date'), np.arcsinh(self.dataset['ps_bucket'].values))
-        # tensor_dict['ps_bucket'] = torch.asinh(tensor_dict['ps_bucket'])
-        ## Prain -> arcsinh
-        self.dataset['pr_bucket'] = (('basin', 'date'), np.arcsinh(self.dataset['pr_bucket'].values))
-        # tensor_dict['pr_bucket'] = torch.asinh(tensor_dict['pr_bucket'])
-        ## M -> arcsinh
-        self.dataset['m_bucket'] = (('basin', 'date'), np.arcsinh(self.dataset['m_bucket'].values))
-        # tensor_dict['m_bucket'] = torch.asinh(tensor_dict['m_bucket'])
-        ## ET -> log(ET / dayl)
-        # Retrieve ET and daylength values from the dataset
-        et_values = self.dataset['et_bucket'].values
-        dayl_values = self.dataset['dayl'].values
-        # Calculate the ratio while avoiding division by zero
-        et_dayl_ratio = np.where(dayl_values == 0, epsilon, et_values / (dayl_values + epsilon))
-        # Ensure no zero or negative values before applying log
-        et_dayl_ratio = np.where(et_dayl_ratio <= 0, epsilon, et_dayl_ratio)
-        # Apply the logarithm transformation
-        self.dataset['et_bucket'] = (('basin', 'date'), np.log(et_dayl_ratio))
-        # et_values = tensor_dict['et_bucket']
-        # dayl_values = tensor_dict['dayl']
-        # et_dayl_ratio = torch.where(dayl_values == 0, epsilon, et_values / (dayl_values + epsilon))
-        # tensor_dict['et_bucket'] = torch.log(torch.where(et_dayl_ratio == 0, epsilon, et_dayl_ratio))
-        ## Q -> log(Q)
-        if not is_trainer:
-            q_values = self.dataset['q_bucket'].values
-            self.dataset['q_bucket'] = (('basin', 'date'), np.log(np.where(q_values == 0, epsilon, q_values)))  # Avoid log(0)
-            # q_values = tensor_dict['q_bucket']
-            # tensor_dict['q_bucket'] = torch.log(torch.where(q_values == 0, epsilon, q_values))
-        else:
+        if is_trainer:
             q_values = self.dataset['obs_runoff'].values
-            self.dataset['obs_runoff'] = (('basin', 'date'), np.log(np.where(q_values == 0, epsilon, q_values)))  # Avoid log(0)
-            # q_values = tensor_dict['obs_runoff']
-            # tensor_dict['obs_runoff'] = torch.log(torch.where(q_values == 0, epsilon, q_values))
+            # self.dataset['obs_runoff'] = (('basin', 'date'), np.log(np.where(q_values == 0, epsilon, q_values)))  # Avoid log(0)
+            self.dataset['obs_runoff'] = (('basin', 'date'), np.log(q_values)) 
+        else:
+            ## Psnow -> arcsinh
+            self.dataset['ps_bucket'] = (('basin', 'date'), np.arcsinh(self.dataset['ps_bucket'].values))
+            ## Prain -> arcsinh
+            self.dataset['pr_bucket'] = (('basin', 'date'), np.arcsinh(self.dataset['pr_bucket'].values))
+            ## M -> arcsinh
+            self.dataset['m_bucket'] = (('basin', 'date'), np.arcsinh(self.dataset['m_bucket'].values))
+            ## ET -> log(ET / dayl)
+            self.dataset['et_bucket'] = (('basin', 'date'), np.log(self.dataset['et_bucket'].values / self.dataset['dayl'].values))
+            ## Q -> log(Q)
+            q_values = self.dataset['q_bucket'].values
+            # self.dataset['q_bucket'] = (('basin', 'date'), np.log(np.where(q_values == 0, epsilon, q_values)))  # Avoid log(0)
+            self.dataset['q_bucket'] = (('basin', 'date'), np.log(q_values))  
 
-        return self.dataset
-        # return tensor_dict
+
+        # return self.dataset
+        # # return tensor_dict
 
     def scale_back_simulated(self, outputs, ds_basin, is_trainer=False):
         # Transfer outputs to CPU if necessary
@@ -820,7 +810,7 @@ class ExpHydroCommon:
         epsilon = 1e-10  # Small constant to avoid division by zero
         et_values = ds_basin['et_bucket'].values
         dayl_values = ds_basin['dayl'].values
-        et_bucket_values = np.exp(et_values) * (dayl_values + epsilon)  # Scale back using exp and dayl
+        et_bucket_values = np.exp(et_values) * dayl_values  # Scale back using exp and dayl
         ds_basin['et_bucket'] = xr.DataArray(et_bucket_values, dims=['date'])
         
         # Q -> log(Q)